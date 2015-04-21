a9a70e5ced5f596ebb1d9f3513e8c7e369447b00
dmanjuna <dmanjuna@asu.edu>
2015-04-21 10:35:23 -0700

    In sync with project phase 2 submission-working code

--- src/main/java/DdsProject/GeospatialOperations/polygonUnion.java
+++ src/main/java/DdsProject/GeospatialOperations/polygonUnion.java
@@ -1,5 +1,6 @@
 package DdsProject.GeospatialOperations;
 
+
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.*;
 import org.apache.spark.SparkConf;
@@ -8,85 +9,46 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Iterator;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.operation.union.CascadedPolygonUnion;
 
 class LocalUnion implements FlatMapFunction<Iterator<String>, Geometry>, Serializable
 {
-	
-	public Iterable<Geometry> call(Iterator<String> lines)
+	public Iterable<Geometry> call(Iterator<String> s)
 	{	
 		List<Geometry> ActivePolygons = new ArrayList<Geometry>();
 		List<Geometry> ReturnPolygons = new ArrayList<Geometry>();
 		GeometryFactory geom = new GeometryFactory();
-        Geometry geoPoly=null;
 
-		lines.next();
-		while(lines.hasNext())
+		while(s.hasNext())
 		{
-			String strTemp = lines.next();
+			String strTemp = s.next();
 			String[] CoordList = strTemp.split(",");
 			Double x1 = Double.parseDouble(CoordList[0]);
 			Double y1 = Double.parseDouble(CoordList[1]);
 			Double x2 = Double.parseDouble(CoordList[2]);
 			Double y2 = Double.parseDouble(CoordList[3]);
 			Geometry poly = geom.createPolygon(new Coordinate[]{new Coordinate(x1,y1),
-															   new Coordinate(x1,y2),
-																new Coordinate(x2,y2),
-																new Coordinate(x2,y1),
-																new Coordinate(x1,y1)});
+					new Coordinate(x1,y2),
+					new Coordinate(x2,y2),
+					new Coordinate(x2,y1),
+					new Coordinate(x1,y1)});
 			ActivePolygons.add(poly);
 		}
-		
-        if(!ActivePolygons.isEmpty())
-        {
-        	geoPoly=ActivePolygons.get(0);
-            ActivePolygons.remove(0);
-        }
-  
-        for( int nI=0;!ActivePolygons.isEmpty();nI++)
-        {
-            if(geoPoly.intersects( ActivePolygons.get(nI)))
-            {
-            	geoPoly = geoPoly.union(ActivePolygons.get(nI));
-                ActivePolygons.remove(nI);
-                if(ActivePolygons.isEmpty())
-                {
-                	ReturnPolygons.add(geoPoly);
-                    break;
-                }   
-                if(!ActivePolygons.isEmpty())
-                {
-                	nI--;
-                }
-                
-                if(nI + 1== ActivePolygons.size())
-                {
-                	nI = 0;
-                }             
-            }
-            else if( !ActivePolygons.isEmpty() && nI + 1==ActivePolygons.size() )
-            {
-            	
-            	ReturnPolygons.add(geoPoly);
-            	geoPoly=ActivePolygons.get(0);
-                ActivePolygons.remove(0);
-                if(!ActivePolygons.isEmpty())
-                {
-                	nI--;
-                }
-                if(ActivePolygons.isEmpty())
-                {
-                    break;
-                }
-                if(ActivePolygons.size() == 1)
-                {
-                	ReturnPolygons.add(geoPoly);
-                }
-            }
-        }
+		Collection<Geometry> pol = ActivePolygons;
+		CascadedPolygonUnion caspol = new CascadedPolygonUnion(pol);
+		Geometry gPols = caspol.union();
+		int nNumGeo = gPols.getNumGeometries();
+
+		for(int nI=0;nI<nNumGeo;nI++)
+		{
+			Geometry pAdd = (Geometry) gPols.getGeometryN(nI);
+			ReturnPolygons.add(pAdd);
+		}
 		return ReturnPolygons;
 	}
 }
@@ -95,76 +57,34 @@ class GlobalUnion implements FlatMapFunction<Iterator<Geometry>, Geometry>, Seri
 {
 	public Iterable<Geometry> call(Iterator<Geometry> givListIter)
 	{	
-		List<Geometry> ActivePolygons = new ArrayList<Geometry>();
+		List<Geometry> polList = new ArrayList<Geometry>();
 		List<Geometry> ReturnPolygons = new ArrayList<Geometry>();
-		Geometry geoPoly=null;
-		
 		while(givListIter.hasNext())
 		{
 			Geometry tempPol = givListIter.next();
-			ActivePolygons.add(tempPol);
+			polList.add(tempPol);
 		}
 
-        if(!ActivePolygons.isEmpty())
-        {
-        	geoPoly=ActivePolygons.get(0);
-        	ActivePolygons.remove(0);
-        }
-  
-        for( int nI=0;!ActivePolygons.isEmpty();nI++)
-        {
-            if(geoPoly.intersects(ActivePolygons.get(nI)) )
-            {
-            	geoPoly = geoPoly.union(ActivePolygons.get(nI));
-                ActivePolygons.remove(nI);
-                if(ActivePolygons.isEmpty())
-                {
-                	ReturnPolygons.add(geoPoly);
-                    break;
-                }   
-                if(!ActivePolygons.isEmpty())
-                {
-                	nI--;
-                }
-     
-                if(nI + 1 == ActivePolygons.size())
-                {
-                	nI = 0;
-                }             
-            }
-            else if( !ActivePolygons.isEmpty() && nI + 1 ==ActivePolygons.size())
-            {
-            	
-            	ReturnPolygons.add(geoPoly);
-            	geoPoly=ActivePolygons.get(0);
-                ActivePolygons.remove(0);
-                if(ActivePolygons.size() == 1)
-                {
-                	ReturnPolygons.add(geoPoly);
-                	break;
-                }
-                if(!ActivePolygons.isEmpty())
-                {
-                	nI--;
-                }
-                if(ActivePolygons.isEmpty())
-                {
-                    break;
-                }
-            }
-        }
+		Collection<Geometry> pol = polList;
+		CascadedPolygonUnion caspol = new CascadedPolygonUnion(pol);
+		Geometry gPols = caspol.union();
+		int nNumGeo = gPols.getNumGeometries();
+
+		for(int nI=0;nI<nNumGeo;nI++)
+		{
+			Geometry pAdd = (Geometry) gPols.getGeometryN(nI);
+			ReturnPolygons.add(pAdd);
+		}
 		return ReturnPolygons;
 	}
 }
 
-public class App
+public class polygonUnion
 {
 	public static void main(String[] args) throws ClassNotFoundException
 	{
 		SparkConf conf = new SparkConf().setAppName("App").setMaster(args[0]);
-		
 		JavaSparkContext sc = new JavaSparkContext(conf);
-				
 		JavaRDD<String> lines = sc.textFile(args[1]);
 		JavaRDD<Geometry> MappedPolygons = lines.mapPartitions(new LocalUnion());
 		MappedPolygons.saveAsTextFile(args[2]);
